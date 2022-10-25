adf83b1648bf572845d7fa49a05404947eaf880f
Bobur Abdullayev <changeitgo3@gmail.com>
2022-10-25 12:02:25 +0500

    feat: function

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/video", (req, res) => {
+  const range = req.headers.Range
+  console.log(range);
+
+  if(!range){
+      res.status(400).send("range is not founded!")
+  }
+
+  const videoSize = fs.statSync(
+    "Eternals.2021 @english_movies_with_subtitle.mkv"
+  ).size;
+  const CHUNK_SIZE = 10 ** 6;
+  const start = Number(range.replace(/\D/g, ""));
+  const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+
+  console.log(CHUNK_SIZE, videoSize, start, end);
+
+  // Create headers
+  const contentLength = end - start + 1;
+  const headers = {
+    "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+    "Accept-Ranges": "bytes",
+    "Content-Length": contentLength,
+    "Content-Type": "video/mp4",
+  };
+
+  // HTTP Status 206 for Partial Content
+  res.writeHead(206, headers);
+
+  // create video read stream for this particular chunk
+  const videoStream = fs.createReadStream(videoPath, { start, end });
+
+  // Stream the video chunk to the client
+  videoStream.pipe(res);
+
+});
+
+const port = 8000;
+app.listen(port, () => {
+  console.log("Server working on port", port);
+});
